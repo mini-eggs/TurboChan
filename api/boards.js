6629106fc7521a8e4e20ba928d99da1e58e712a4
Evan M Jones <evan@thinkpad.thinkpad>
2018-01-14 17:13:34 -0800

    removing non worksafe boards

--- api/boards.js
+++ api/boards.js
@@ -3,8 +3,11 @@ import Axios from "axios";
 
 const router = Router();
 
+const worksafeOnly = ({ ws_board }) => ws_board === 1;
+
 router.get("/boards", async (_, res) => {
   const next_res = await Axios("https://a.4cdn.org/boards.json");
+  next_res.data.boards = next_res.data.boards.filter(worksafeOnly);
   res.json(next_res.data);
 });
 
